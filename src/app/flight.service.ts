9c8a1ea94dfe9b11e0ca5e0200f982603f52d687
mateuszstaszkow <mateuszstasz@gmail.com>
2021-01-16 03:57:15 +0100

    fix: Move API calls to the backend

---
+++ src/app/flight.service.ts
@@ -0,0 +1,20 @@
+import {Injectable} from '@angular/core';
+import {Flight} from './model';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {environment} from '../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FlightService {
+
+  private readonly baseUrl = environment.baseUrl;
+
+  constructor(private readonly httpClient: HttpClient) {
+  }
+
+  public getFlights(): Observable<Flight[]> {
+    return this.httpClient.get<Flight[]>(this.baseUrl + '/flights');
+  }
+}
